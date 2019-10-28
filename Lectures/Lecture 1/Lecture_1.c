505e26e693b19177165b02fd0627396f5fe79553
george-sarji <georgesarji@gmail.com>
2019-10-28 12:30:20 +0200

    Changed project structure, added first lecture files (basic intro)

---
+++ Lectures/Lecture 1/Lecture_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	//This will cover the first lecture, which includes the IO interactions (printf), conditionals (if/else/switches) and variable assignments
+
+	return 0;
+}
