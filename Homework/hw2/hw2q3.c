0d1afc346cabbbcd6e3dc703e048c70dbadf74cb
george-sarji <georgesarji@gmail.com>
2019-11-21 12:49:39 +0200

    Added documentation in HW2Q3

--- Homework/hw2/hw2q3.c
+++ Homework/hw2/hw2q3.c
@@ -10,6 +10,7 @@
 ==========================================================================*/
 
 /* put your #defines and typedefs here*/
+// The provided max size of the matrix in the question is 25.
 #define MAX_SIZE 25
 
 /*-------------------------------------------------------------------------
@@ -32,6 +33,9 @@ int handle_operations(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MA
 
 int main()
 {
+	// This function will handle the main calls required by the code to properly work.
+	// The first thing the function does is receive the size of the matrices. If the input is invalid,
+	// an error is printed to the console and the program will exit.
 	print_enter_matrix_size();
 	int matrix_size = 0, first_matrix[MAX_SIZE][MAX_SIZE], second_matrix[MAX_SIZE][MAX_SIZE];
 	if (!scanf("%d", &matrix_size))
@@ -39,12 +43,17 @@ int main()
 		print_invalid_input();
 		return 0;
 	}
-
+	// The conditional here will call the receive matrices function, which receives the two required matrices.
+	// If the function returns EOF, an invalid input message is shown and the program exits.
+	// For further clarification, review the receive matrices function.
 	if (receive_matrices(first_matrix, second_matrix, matrix_size) == EOF)
 	{
 		print_invalid_input();
 		return 0;
 	}
+	// This conditional calls the function responsible for handling the operator that is input by the user.
+	// In the case that the user inputs an incorrect symbol, an invalid input message is shown and the program exits.
+	// For further clarification, review the handle operations function.
 	if (handle_operations(first_matrix, second_matrix, matrix_size) == EOF)
 	{
 		print_invalid_input();
@@ -88,13 +97,20 @@ void print_enter()
 	printf("\n");
 }
 
+// This function is in charge of receiving the two matrices that the user provides.
+// As seen, the two matrices are provided within the arguements of the function, as you can not return arrays from functions.
 int receive_matrices(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int matrix_size)
 {
+	// The function requests the user to input the first matrix.
 	print_enter_first_matrix();
+	// The generic function receive_matrix performs the logic required to receive the matrix per the requirements of the question.
+	// Review the receive matrix function for further clarification.
+	// If the receive matrix function returns EOF, this whole function will return EOF to print an invalid input message.
 	if (receive_matrix(matrix_size, first_matrix) == EOF)
 	{
 		return EOF;
 	}
+	// Same as earlier, but the section here receives the second matrix.
 	print_enter_second_matrix();
 	if (receive_matrix(matrix_size, second_matrix) == EOF)
 	{
@@ -103,8 +119,11 @@ int receive_matrices(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX
 	return 1;
 }
 
+// This function is in charge of recording the input of the user and constructing the matrix properly according to the provided size.
+// The function receives the requested size of the matrix, along with the matrix that will be edited to host the input.
 int receive_matrix(int size, int matrix[MAX_SIZE][MAX_SIZE])
 {
+	// The function goes from left to right, top to bottom in order for the input to be added into the matrix.
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -112,6 +131,7 @@ int receive_matrix(int size, int matrix[MAX_SIZE][MAX_SIZE])
 			// Receive the number and assign into the matrix.
 			if (!scanf("%d", &matrix[i][j]))
 			{
+				// The input was not a valid number. Function will return EOF to trigger an invalid input message.
 				return EOF;
 			}
 		}
@@ -119,8 +139,11 @@ int receive_matrix(int size, int matrix[MAX_SIZE][MAX_SIZE])
 	return 0;
 }
 
+// This function, as mentioned in the name of it, is in charge of multiplying two matrices.
+// The function receives two matrices in the arguements, alongside the size of the matrices.
 void mutiply_matrices(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int size)
 {
+	// The function goes from left to right, top to bottom in order to multiple the matrices.
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -128,35 +151,48 @@ void mutiply_matrices(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MA
 			// We arrived at a(i, j). We need to perform the multiplication between the two matrices.
 			multiply_row_column(first_matrix, second_matrix, size, i, j);
 		}
+		// Go down a new line since we finished the row.
 		print_enter();
 	}
 }
 
+// This function handles the individual multiplication of a specific row and column from each matrix.
 void multiply_row_column(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int size, int row, int column)
 {
+	// We define a sum that is used to add up the resultant of the multiplications.
 	int sum = 0;
+	// This for loop will iterate through the matrix. (e.g: a11*b11, a21 * b12, etc.)
 	for (int element = 0; element < size; element++)
 	{
 		sum += first_matrix[row][element] * second_matrix[element][column];
 	}
+	// We have finished the multiplication, print the sum.
 	print_number(sum);
 }
 
+// This function is in charge of adding up two matrices to each other.
 void add_matrices(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int size)
 {
+	// Left to right, top to bottom sequence.
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
+			// Print the resultant of the addition.
 			print_number(first_matrix[i][j] + second_matrix[i][j]);
 		}
+		// Go down a new line as the row is complete
 		print_enter();
 	}
 }
+
+// This function is in charge of handling the operation input by the user.
+// The function receives two matrices, alongside their size in the arguements.
 int handle_operations(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int matrix_size)
 {
 	print_enter_required_operation();
 	char operation;
+	// Receive the required operation and check whether it is legal or not.
 	if (scanf(" %c", &operation) && (operation == '+' || operation == '*'))
 	{
 		// perform the required operation
@@ -169,6 +205,7 @@ int handle_operations(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MA
 			add_matrices(first_matrix, second_matrix, matrix_size);
 		}
 	}
+	// The input was either 1) not castable to a char or 2) not + or *, thus return EOF to trigger an invalid input message.
 	else
 	{
 		return EOF;
