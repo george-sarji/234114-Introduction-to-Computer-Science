b47294697c52984ea28a46b19ba9175150c9ea82
george-sarji <georgesarji@gmail.com>
2019-11-21 13:04:52 +0200

    Added documentation into HW2Q2

--- Homework/hw2/hw2q2.c
+++ Homework/hw2/hw2q2.c
@@ -10,7 +10,9 @@
 ==========================================================================*/
 
 /* put your #defines and typedefs here*/
+// The max number of platoons.
 #define PLATOONS 10
+// The number for each operation/demand
 #define SUM_OPT 1
 #define BIGGEST_PLAT 2
 #define ACTIVE_NUM 3
@@ -46,6 +48,9 @@ void handle_option_3(int platoon[PLATOONS]);
 void print_army(int platoons[PLATOONS], int counter);
 void handle_army_details(int counter, int platoons[PLATOONS], int demand);
 void receive_numbers();
+void handle_demand(int counter, int platoons[PLATOONS], int demand);
+void print_army_header();
+int print_army_row(int platoons[PLATOONS]);
 
 int main()
 {
@@ -102,58 +107,43 @@ void print_space()
 void receive_numbers()
 {
 	// This function will receive the numbers of the soldiers and the demand at the end.
+	// Initialize the platoons array as a counter array.
 	int currentNumber = 0, currentAssignment = 0, counter = 0, platoons[PLATOONS] = {0};
+	// Scan for 3 digit numbers and place them in currentNumber. Check whether the number is actually a 1 digit number (highlights a demand).
+	// If the number is not a 1 digit number, continue on with processing the warrior IDs.
 	while (scanf("%3d", &currentNumber) && currentNumber > 10)
 	{
+		// Add into the main army size counter.
 		counter++;
 		// Calculate assignment.
 		currentAssignment = ((currentNumber / 100) % 10 + (currentNumber / 10) % 10 + currentNumber % 10) % 10;
+		// Add 1 into the assigned platoon counter.
 		platoons[currentAssignment]++;
 	}
 	// The current number at this point will be the demand of the commander.
-	switch (currentNumber)
-	{
-	case SUM_OPT:
-	{
-		print_army_size(counter);
-		break;
-	}
-	case BIGGEST_PLAT:
-	{
-		handle_option_2(platoons);
-		break;
-	}
-
-	case ACTIVE_NUM:
-	{
-		handle_option_3(platoons);
-		break;
-	}
-
-	case PRINT_PLAT:
-	{
-		print_army(platoons, counter);
-		break;
-	}
-	}
+	handle_demand(counter, platoons, currentNumber);
 }
 
 void handle_option_2(int platoons[PLATOONS])
 {
 	// This function will handle the biggest platoons and its sum.
 	int biggest_platoon = 0;
+	// By default wwe have platoon that has the biggest size. Thus, we start at index 0.
 	for (int i = 0; i < PLATOONS; i++)
 	{
+		// As we want the biggest index of the biggest platoon, if the biggest platoon's size is the same as the current, switch the index.
 		if (platoons[i] >= platoons[biggest_platoon])
 		{
 			biggest_platoon = i;
 		}
 	}
+	// Call the provided function.
 	print_biggest_platoon_and_size(biggest_platoon, platoons[biggest_platoon]);
 }
 
 void handle_option_3(int platoons[PLATOONS])
 {
+	// Define a counter and a sum in order to provide an average.
 	double counter = 0, sum = 0;
 	for (int i = 0; i < PLATOONS; i++)
 	{
@@ -163,31 +153,85 @@ void handle_option_3(int platoons[PLATOONS])
 			sum += platoons[i];
 		}
 	}
+	// Call the provided function.
 	print_active_platoons_and_avg(counter, sum / counter);
 }
 
 void print_army(int platoons[PLATOONS], int counter)
 {
+	// Print the show army text (provided function)
 	print_show_army();
+	// Print the army header (0123456789)
+	print_army_header();
+	// While there are warriors left over and not printed, print a row of the platoons.
+	while (counter > 0)
+	{
+		counter -= print_army_row(platoons);
+	}
+}
+
+void handle_demand(int counter, int platoons[PLATOONS], int demand)
+{
+	// Handle the demand of the commander.
+	switch (demand)
+	{
+	case SUM_OPT:
+	{
+		// Print the size of the army
+		print_army_size(counter);
+		break;
+	}
+	case BIGGEST_PLAT:
+	{
+		// Print the biggest platoon index and size
+		handle_option_2(platoons);
+		break;
+	}
+
+	case ACTIVE_NUM:
+	{
+		// Print the number of active platoons and their average size
+		handle_option_3(platoons);
+		break;
+	}
+
+	case PRINT_PLAT:
+	{
+		// Print the whole army divided into platoons.
+		print_army(platoons, counter);
+		break;
+	}
+	}
+}
+
+// This function prints the header of the army printing section (0123456789).
+void print_army_header()
+{
 	for (int i = 0; i < PLATOONS; i++)
 	{
 		print_number(i);
 	}
-	while (counter > 0)
+	print_enter();
+}
+
+// This function is in charge of printing the individual rows of the platoons (stars) depicting the number of warrios in the platoon.
+// The function will return the amount of times a star was printed in the current row iteration (used for counter calculation)
+int print_army_row(int platoons[PLATOONS])
+{
+	int print_count = 0;
+	for (int i = 0; i < PLATOONS; i++)
 	{
-		print_enter();
-		for (int i = 0; i < PLATOONS; i++)
+		if (platoons[i] > 0)
 		{
-			if (platoons[i] > 0)
-			{
-				platoons[i]--;
-				counter--;
-				print_star();
-			}
-			else
-			{
-				print_space();
-			}
+			platoons[i]--;
+			print_count++;
+			print_star();
+		}
+		else
+		{
+			print_space();
 		}
 	}
+	print_enter();
+	return print_count;
 }
