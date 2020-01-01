0d033e2162cfa35776698f95047d42f08723264a
george-sarji <georgesarji@gmail.com>
2020-01-01 18:39:39 +0200

    Added complexity calculations to question 3

--- Homework/hw4/hw4q3.c
+++ Homework/hw4/hw4q3.c
@@ -7,22 +7,22 @@ int main()
     scanf("%d", &question);
     switch (question) {
     case 1:
-        printf("The answer to question 1 is a\n");
+        printf("The answer to question 1 is h\n");
         break;
     case 2:
-        printf("The answer to question 2 is a\n");
+        printf("The answer to question 2 is g\n");
         break;
     case 3:
-        printf("The answer to question 3 is a\n");
+        printf("The answer to question 3 is k\n");
         break;
     case 4:
         printf("The answer to question 4 is a\n");
         break;
     case 5:
-        printf("The answer to question 5 is a\n");
+        printf("The answer to question 5 is h\n");
         break;
     case 6:
-        printf("The answer to question 6 is a\n");
+        printf("The answer to question 6 is d\n");
         break;
     default:
         printf("Error!\n");
