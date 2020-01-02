b08d4ddde8e067f8eee8783a59f4d1743cf19e37
george-sarji <georgesarji@gmail.com>
2020-01-02 23:51:45 +0200

    Completed hw5q1

--- Homework/hw5/hw5q1.c
+++ Homework/hw5/hw5q1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 void PrintWellcomeMessage()
 {
@@ -11,5 +13,43 @@ void PrintIsPalindrome()
 }
 void PrintIsNotPalindrome()
 {
-	printf("It's not a palindrome!");
+    printf("It's not a palindrome!");
+}
+
+bool CheckPalindrome(int palindrome, int originalNum, int sum);
+int DigitCount(int number);
+
+int main()
+{
+    PrintWellcomeMessage();
+    int number = 0;
+    scanf("%d", &number);
+    if (CheckPalindrome(number, number, 0))
+    {
+        PrintIsPalindrome();
+    }
+    else
+    {
+        PrintIsNotPalindrome();
+    }
+    return 1;
+}
+
+int DigitCount(int number)
+{
+    if (number % 10 == 0)
+        return 0;
+    return 1 + DigitCount(number / 10);
+}
+
+bool CheckPalindrome(int palindrome, int originalNum, int sum)
+{
+    if (palindrome != 0)
+    {
+        sum = sum * 10 + palindrome % 10;
+        return CheckPalindrome(palindrome /= 10, originalNum, sum);
+    }
+    else if (sum == originalNum)
+        return true;
+    return false;
 }
