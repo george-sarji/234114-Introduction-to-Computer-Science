028a7cf5ec6d328e7dd7951d8eef26fcd13d61bb
george-sarji <georgesarji@gmail.com>
2020-01-06 23:25:31 +0200

    Added second version of hw5q1 with the use of math.h

--- Homework/hw5/hw5q1.c
+++ Homework/hw5/hw5q1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 void PrintWellcomeMessage()
 {
@@ -18,13 +19,23 @@ void PrintIsNotPalindrome()
 
 bool CheckPalindrome(int palindrome, int originalNum, int sum);
 int DigitCount(int number);
+bool CheckPalindrome2(int palindrome, int digitCount);
 
 int main()
 {
     PrintWellcomeMessage();
     int number = 0;
     scanf("%d", &number);
-    if (CheckPalindrome(number, number, 0))
+
+    // if (CheckPalindrome(number, number, 0))
+    // {
+    //     PrintIsPalindrome();
+    // }
+    // else
+    // {
+    //     PrintIsNotPalindrome();
+    // }
+    if (CheckPalindrome2(number, DigitCount(number)))
     {
         PrintIsPalindrome();
     }
@@ -37,7 +48,7 @@ int main()
 
 int DigitCount(int number)
 {
-    if (number % 10 == 0)
+    if (number == 0)
         return 0;
     return 1 + DigitCount(number / 10);
 }
@@ -52,4 +63,14 @@ bool CheckPalindrome(int palindrome, int originalNum, int sum)
     else if (sum == originalNum)
         return true;
     return false;
+}
+
+bool CheckPalindrome2(int palindrome, int digitNumber)
+{
+    if (digitNumber <= 0)
+        return true;
+    int currentDigitPow = pow(10, digitNumber - 1);
+    int firstNum = palindrome % 10, secondNum = palindrome / currentDigitPow;
+    // printf("Received num: %d, comparing %d = %d\n", palindrome, firstNum, secondNum);
+    return firstNum == secondNum && CheckPalindrome2((palindrome % currentDigitPow / 10), digitNumber - 2);
 }
